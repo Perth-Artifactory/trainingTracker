81bb283530071ba43139e256c51c9065131cabfd
Fletcher <me@fboyd.me>
2024-08-12 15:37:46 +0800

    Add checkin explainers

--- util/strings.py
+++ util/strings.py
@@ -39,5 +39,16 @@ trainer_no_tools = "This user is not authorised to use any medium or high risk t
 trainer_add_explainer = "Check the boxes next to the tools you want to authorise this user to use. Tools the user is already authorised to use are not listed."
 trainer_remove_explainer = "Check the boxes next to the tools you want to remove this user's authorisation to use. Tools the user is not authorised to use are not listed."
 
-check_in_explainer_trainer = "This tool needs a follow up with the operator after {} days. You can check in with the operator using the buttons below. I will send you a reminder if you haven't checked in with the operator after by: {}"
+check_in_explainer_trainer = "This tool needs a follow up with the operator after {} days. A trainer can check in with the operator using the buttons below. I will send the original trainer a reminder if this isn't actioned by: {}"
 check_in_no_slack = "This user does not have an associated Slack account so I can't open a direct message with them. A committee member can contact the operator <https://artifactory.tidyhq.com/contacts/{}|here>."
+
+checkin_explainer_operator = """This is in relation to the **{}** induction you completed {} days ago.
+We've found that a key part of the learning process is to put the knowledge you've learned into practice soon after its been acquired. For certain inductions this involves a check in with a trainer to ensure you've had an opportunity to use the tool, cement your learning, and ask any questions you may have.
+
+Please indicate:
+
+* If you've used the tool since your induction
+* If you have any questions or concerns"""
+
+checkin_induction_approved = "Your **{}** induction has been maintained. Unless something changes about the tool or we identify it's been a long time since you've used it I won't contact you about it again."
+checkin_induction_rejected = "Unfortunately your **{}** induction has been revoked. Please arrange with a trainer to undergo a refresher induction before using this tool again."
